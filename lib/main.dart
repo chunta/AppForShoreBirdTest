52dd01b18da61ac274c9df5ca0c44ce0f2384c92
Rex Chen <rexchen@RexdeMac-mini.local>
2024-07-29 20:22:53 +0800

    patch 1 for 1.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orangeAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black87),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo 1.0.4'),
